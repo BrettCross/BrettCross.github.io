4a477ea51b17e3c4a6d3dae65610536a8a3fee07
BrettCross <brettcross@cmail.carleton.ca>
2024-08-26 19:45:50 -0400

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 //import './App.css';
 import React from "react";
 import About from './components/About';
-import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
@@ -10,6 +9,7 @@ function App() {
   return (
     <main>
       <div className="text-gray-400 bg-gray-900 body-font">
+        <Navbar />
         <About />
         <Projects />
         <Skills />
